b99fe59c66e3a55dde9479a3af1c4baf2253db81
Boringahhh <913493911@qq.com>
2026-02-03 16:36:25 +0800

    feat✨: add singbox_auto.js and new singbox.json

---
+++ singbox/singbox.js
@@ -0,0 +1,56 @@
+const { type, name } = $arguments
+const compatible_outbound = {
+  tag: 'COMPATIBLE',
+  type: 'direct',
+}
+
+let compatible
+let config = JSON.parse($files[0])
+let proxies = await produceArtifact({
+  name,
+  type: /^1$|col/i.test(type) ? 'collection' : 'subscription',
+  platform: 'sing-box',
+  produceType: 'internal',
+})
+
+config.outbounds.push(...proxies)
+
+config.outbounds.map(i => {
+  if (['🚀 手动切换'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies))
+  }
+  if (['🇭🇰 香港节点'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /港|hk|hongkong|hong kong|🇭🇰/i))
+  }
+  if (['🇨🇳 台湾节点'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /台|tw|taiwan|🇹🇼/i))
+  }
+  if (['🇯🇵 日本节点'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /日本|jp|japan|🇯🇵/i))
+  }
+  if (['🇸🇬 新加坡节点'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /^(?!.*(?:us)).*(新|sg|singapore|🇸🇬)/i))
+  }
+  if (['🇺🇲 美国节点'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /美|us|unitedstates|united states|🇺🇸/i))
+  }
+  if (['🇰🇷 韩国节点'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /韩国|KR|KOR|Korea|korea|KOREA|🇰🇷/i))
+  }
+})
+
+config.outbounds.forEach(outbound => {
+  if (Array.isArray(outbound.outbounds) && outbound.outbounds.length === 0) {
+    if (!compatible) {
+      config.outbounds.push(compatible_outbound)
+      compatible = true
+    }
+    outbound.outbounds.push(compatible_outbound.tag);
+  }
+});
+
+$content = JSON.stringify(config, null, 2)
+
+function getTags(proxies, regex) {
+  return (regex ? proxies.filter(p => regex.test(p.tag)) : proxies).map(p => p.tag)
+}
